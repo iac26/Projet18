263b2b2f491155ec1380531f2564ac30909abd8a
lianyi66 <36849370+lianyi66@users.noreply.github.com>
2018-03-01 12:04:09 +0100

    CONSTANTE.H

---
+++ constantes.h
@@ -0,0 +1,16 @@
+#include "tolerance.h"
+
+#define DELTA_T 0.25
+#define VTRAN_MAX 0.75
+#define VROT_MAX 0.5
+#define DELTA_VROT 0.125
+#define DELTA_TRAN 0.25
+#define DMAX 20
+#define R_ROBOT 0.5
+#define R_PARTICULE_MAX 4
+#define R_PARTICULE_MIN 0.3
+#define R_PARTICULE_FACTOR 0.4142
+#define E_PARTICULE_MAX 1
+#define E_PARTICULE_FACTOR 0.25
+#define DECOMPOSITION_RATE 0.025
+#define MAX_LINE 120
