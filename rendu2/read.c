b61698f67414e93dd09f2d84fb528de1dc7045bf
iac26 <iacopo.sprenger@epfl.ch>
2018-03-13 02:02:27 +0100

    lol

--- rendu2/read.c
+++ rendu2/read.c
@@ -23,44 +23,30 @@ int main (int argc, char ** argv) {
 static int read_file(char * filename) {
 	FILE * file = fopen(filename, "r");
 	char line[MAX_LN_SIZE];
-	unsigned short comment_state;
 	unsigned short reader_state = NB_ROBOT;
 	int nb_robot;
 	int nb_particle;
 	int robot_count = 0;
 	int particle_count = 0;
-	int align;
-	char * token;
-	double robot_x, robot_y, robot_a;
-	double particle_x, particle_y, particle_r, particle_e;
 	if (file) {
 		while(fgets(line, MAX_LN_SIZE, file) != NULL) {
-			comment_state = CHECKING;
 			if((line[0]=='\n')||(line[0]=='\r')||(line[0]=='#')) {
 				continue;
 			}
-			for(int i = 0; i < MAX_LN_SIZE; i++) {
-				if(line[i] == '#') {
-					comment_state = ERASING;
-				}
-				if(comment_state == ERASING) {
-					line[i] = ' ';
-				}
-			}
-			char fin[10];
-			sscanf(line, "%10s", fin);
-			if(!strcmp(fin, "FIN_LISTE")) {
-				if(reader_state == ROBOT) {
-					if(robot_count != nb_robot) {
-						error_invalid_nb_robots();
-					}
-					reader_state = NB_PARTICLE;
-				}
-				if(reader_state == PARTICLE) {
-					if(particle_count != nb_particle) {
-						error_invalid_nb_particules();
-					}
-					reader_state = END;
+			if(detect_fin_liste(line)) {
+				switch (reader_state) {
+					case ROBOT:
+						if(robot_count != nb_robot) {
+							error_invalid_nb_robots();
+						}
+						reader_state = NB_PARTICLE;
+						break;
+					case PARTICLE: 
+						if(particle_count != nb_particle) {
+							error_invalid_nb_particules();
+						}
+						reader_state = END;
+						break;
 				}
 				continue;
 			}
@@ -72,30 +58,7 @@ static int read_file(char * filename) {
 					}
 					break;
 				case ROBOT:
-					token = strtok(line, " ");
-					align = 0;
-					while(token) {
-						double tmp;
-						if(sscanf(token, "%lf", &tmp) == 1) {
-							switch (align) {
-								case 0:
-									robot_x = tmp;
-									break;
-								case 1:
-									robot_y = tmp;
-									break;
-								case 2:
-									robot_a = tmp;
-									break;
-							}
-							align++;
-						}
-						if(align == 3) {
-							printf("robot %g %g %g\n", robot_x, robot_y, robot_a);
-							robot_count++;
-						}
-						token = strtok(NULL, " ");
-					}
+					read_robot(line, &robot_count);
 					break;
 				case NB_PARTICLE:
 					if(sscanf(line, "%d", &nb_particle) == 1) {
@@ -104,34 +67,7 @@ static int read_file(char * filename) {
 					}
 					break;
 				case PARTICLE:
-					token = strtok(line, " ");
-					align = 0;
-					while(token) {
-						double tmp;
-						if(sscanf(token, "%lf", &tmp) == 1) {
-							switch (align) {
-								case 0:
-									particle_e = tmp;
-									break;
-								case 1:
-									particle_r = tmp;
-									break;
-								case 2:
-									particle_x = tmp;
-									break;
-								case 3:
-									particle_y = tmp;
-									break;
-							}
-							align++;
-						}
-						if(align == 4) {
-							printf("particle %g %g %g %g\n", particle_e, particle_r, particle_x, particle_y);
-							particle_count++;
-							align = 0;
-						}
-						token = strtok(NULL, " ");
-					}
+					read_particle(line, &particle_count);
 					break;
 				case END:
 					break;
@@ -142,4 +78,88 @@ static int read_file(char * filename) {
 		error_file_missing(filename);
 	}
 	return 1;
+}
+
+static void read_robot(char * line, int * p_robot_count) {
+	double robot_x, robot_y, robot_a;
+	int align = 0;
+	char * token = strtok(line, " ");
+	while(token) {
+		double tmp;
+		if(sscanf(token, "%lf", &tmp) == 1) {
+			switch (align) {
+				case 0:
+					robot_x = tmp;
+					break;
+				case 1:
+					robot_y = tmp;
+					break;
+				case 2:
+					robot_a = tmp;
+					break;
+			}
+			align++;
+		}
+		if(align == 3) {
+			printf("robot %g %g %g\n", robot_x, robot_y, robot_a);
+			if(p_robot_count)
+				*p_robot_count += 1;
+		}
+		token = strtok(NULL, " ");
+	}
+}
+
+static void read_particle(char * line, int * p_particle_count) {
+	double particle_x, particle_y, particle_r, particle_e;
+	int align = 0;
+	char * token = strtok(line, " ");
+	while(token) {
+		double tmp;
+		if(sscanf(token, "%lf", &tmp) == 1) {
+			switch (align) {
+				case 0:
+					particle_e = tmp;
+					break;
+				case 1:
+					particle_r = tmp;
+					break;
+				case 2:
+					particle_x = tmp;
+					break;
+				case 3:
+					particle_y = tmp;
+					break;
+			}
+			align++;
+		}
+		if(align == 4) {
+			printf("particle %g %g %g %g\n", particle_e, particle_r, particle_x, particle_y);
+			if(p_particle_count)
+				*p_particle_count += 1;
+			align = 0;
+		}
+		token = strtok(NULL, " ");
+	}
+}
+
+static void remove_comments(char * line) {
+	unsigned short comment_state = CHECKING;
+	for(int i = 0; i < MAX_LN_SIZE; i++) {
+		if(line[i] == '#') {
+			comment_state = ERASING;
+		}
+		if(comment_state == ERASING) {
+			line[i] = ' ';
+		}
+	}
+}
+
+static int detect_fin_liste(char * line) {
+	char str[10];
+	sscanf(line, "%10s", str);
+	if(!strcmp(str, "FIN_LISTE")) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
--- rendu2/read.h
+++ rendu2/read.h
@@ -1,6 +1,10 @@
 #ifndef READ_H
 #define READ_H
 
-static int read_file(char * filename); 
+static int read_file(char * filename);
+static void read_robot(char * line, int * p_robot_count);
+static void read_particle(char * line, int * p_particle_count);
+static void remove_comments(char * line);
+static int detect_fin_liste(char * line);
 
 #endif
