f56cb569ca4885407eddc5563e82d449df4402bb
iac26 <iacopo.sprenger@epfl.ch>
2018-03-25 20:09:19 +0200

    modif

---
+++ rendu2_lianyi/read.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <constantes.h>
+#include <error.h>
+#include <tolerance.h>
+#include <read.h>
+
+int main(int argc, char * argv[]){
+	FILE * read = NULL;
+	read = fopen(argv[1],'r');
+	
+}
